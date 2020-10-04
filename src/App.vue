c09cbd8cdae1c668b3867cebc3d8a8dde8c3b5ae
wujihua <wjh50940@163.com>
2020-10-04 22:10:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <tabs :currentIndex="currentIndex" @changeIndex="getChangeIndex">
+      <tab label="主页" index="1">
+        <div>主页</div>
+      </tab>
+      <tab label="分类" index="2">
+        <div>分类</div>
+      </tab>
+      <tab label="标签" index="3">
+        <div>标签</div>
+      </tab>
+      <tab label="我的" index="4">
+        <div>我的</div>
+      </tab>
+    </tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      currentIndex: '1'
+    }
+  },
+  methods: {
+    getChangeIndex (index) {
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/tabs/tab.vue
@@ -0,0 +1,66 @@
+<script>
+export default {
+  name: 'Tab',
+  data () {
+    return {}
+  },
+  props: {
+    label: {
+      type: [String, Number],
+      default: 'tab'
+    },
+    index: {
+      type: [String, Number],
+      default: '1'
+    }
+  },
+  computed: {
+    // 索引相等时高亮
+    active () {
+      return this.index == this.$parent.currentIndex
+    }
+  },
+  methods: {
+    clickHandler () {
+      this.$parent.onChangeIndex(this.index)
+    }
+  },
+  mounted () {
+    // this => 当前组件（tab）
+    this.$parent.panes.push(this)
+  },
+  render () {
+    const currentClass = {
+      tab: true,
+      active: this.active
+    }
+    return (
+      <li class={ currentClass } onClick={ this.clickHandler }>{ this.label }</li>
+    )
+  }
+}
+</script>
+
+<style scoped>
+  .tab {
+    flex: 1;
+    list-style: none;
+    line-height: 50px;
+    position: relative;
+    text-align: center;
+    cursor: pointer;
+    margin-right: 5px;
+  }
+  .tab:hover {
+    background-color: #2296F3;
+    color: #fff;
+  }
+  .tab:last-child {
+    margin-right: 0;
+  }
+
+  .tab.active {
+    color: #fff;
+    background-color: #2296F3;
+  }
+</style>
